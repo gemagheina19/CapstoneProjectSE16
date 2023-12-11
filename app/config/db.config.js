40542d3f6bd79be994e254602930369846a23ee4
gemagheina19 <gheina.gemalia@ui.ac.id>
2023-12-11 22:46:19 +0700

    di aku yg ini work (?)

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,11 +2,7 @@ module.exports = {
     HOST: 'localhost',
     USER: 'root',
     PASSWORD: '',
-<<<<<<< HEAD
-    DB: "livingspace",
-=======
-    DB: 'living_space',
->>>>>>> 453e77f01cce59c17f722b8212631ec77579f12f
+    DB: 'livingspace',
     dialect: 'mysql',
     pool: {
         max: 5,
