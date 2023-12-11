c36da1a27b7abde1938fd8207ebf153d1d9344ca
wahyutbp28 <wahyutbp28@gmail.com>
2023-12-11 19:18:44 +0700

    API CITIES

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const cors = require('cors');
-const cityRoute = require('./routes/city.routes');
+const cityyRoute = require('./app/routes/city.routes');
 
-const cityRoutes = require('./routes/cityRoutes');
-const sequelize = require('./config/database');
 
 
 
@@ -40,12 +38,11 @@ app.use(express.static('public'));
 app.use(cors());
 
 
-
-
-const db = require('./models');
+const db = require('./app/models');
 db.sequelize.sync();
 
-app.use('/api/cities', cityRoute);
+
+
 
 
 //KOTA STATIC API
@@ -56,6 +53,9 @@ app.post('/kota', kotaController.createKota);
 app.put('/kota/:id', kotaController.updateKotaById);
 app.delete('/kota/:id', kotaController.deleteKotaById);
 
+
+
+app.use('/api/cities', cityyRoute);
 
 app.listen(port, () => {
   console.log(`Server berjalan di http://localhost:${port}`);
